dc7a57b47f9ede693991f520b7fd95af9a8238a7
gisehweb <gisehbc@gmail.com>
2021-11-24 09:09:08 -0300

    habilite gii, para crear los archivos que necesite usuario, entre al model generator luego al crud y comprobe que se vea en usuarioindex

---
+++ src/basic/models/Usuario.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "usuario".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $nombre
+ * @property int $apellido
+ */
+class Usuario extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'usuario';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'nombre', 'apellido'], 'required'],
+            [['apellido'], 'integer'],
+            [['username'], 'string', 'max' => 50],
+            [['nombre'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'nombre' => 'Nombre',
+            'apellido' => 'Apellido',
+        ];
+    }
+}
